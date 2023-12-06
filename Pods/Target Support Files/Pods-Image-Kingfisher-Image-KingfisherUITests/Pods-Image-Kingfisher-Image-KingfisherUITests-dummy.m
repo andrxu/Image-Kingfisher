db3caea16c076e10f78fd0de62cc1f6f0b0f9c5c
Andrew Xu <andrew.xu@hbo.com>
2023-12-05 22:27:38 -0800

    minor updates

--- Pods/Target Support Files/Pods-Image-Kingfisher-Image-KingfisherUITests/Pods-Image-Kingfisher-Image-KingfisherUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Image_Kingfisher_Image_KingfisherUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Image_Kingfisher_Image_KingfisherUITests
-@end
--- Pods/Target Support Files/Pods-Image-KingfisherTests/Pods-Image-KingfisherTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Image_KingfisherTests : NSObject
-@end
-@implementation PodsDummy_Pods_Image_KingfisherTests
-@end
